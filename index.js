0de9f6a5a9c02e15c9196b2fc3be8e851d1fdaad
Qiu Xiang <i@7c00.cc>
2017-05-26 01:06:32 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,3 @@
+import {NativeModules} from 'react-native'
+const {AMap3D} = NativeModules
+export default AMap3D
---
+++ ios/AMap3D.m
@@ -0,0 +1,11 @@
+#import "AMap3D.h"
+
+@implementation AMap3D
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
