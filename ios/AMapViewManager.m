f75bfb8846f563f417e2bf954127860511d3d15c
Qiu Xiang <i@7c00.cc>
2017-06-13 17:29:34 +0800

    实现 iOS 地图类型接口

--- ios/AMapViewManager.m
+++ ios/AMapViewManager.m
@@ -5,30 +5,55 @@
 @interface AMapViewManager : RCTViewManager
 @end
 
-@implementation AMapViewManager
+@implementation RCTConvert (AMapView)
+RCT_ENUM_CONVERTER(MAMapType, (@{
+        @"standard": @(MAMapTypeStandard),
+        @"satellite": @(MAMapTypeSatellite),
+        @"navigation": @(MAMapTypeNavi),
+        @"night": @(MAMapTypeStandardNight),
+        @"bus": @(MAMapTypeBus),
+}), MAMapTypeStandard, integerValue)
+@end
+
+@implementation AMapViewManager {
+    AMapView *mapView;
+}
 
 RCT_EXPORT_MODULE()
 
-- (UIView *)view
-{
-    AMapView *mapView = [[AMapView alloc] init];
+- (UIView *)view {
+    mapView = [[AMapView alloc] init];
     mapView.centerCoordinate = CLLocationCoordinate2DMake(39.9042, 116.4074);
     mapView.zoomLevel = 10;
     return mapView;
 }
 
 RCT_EXPORT_VIEW_PROPERTY(showsCompass, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(showsScale, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(showsIndoorMap, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(showsLabels, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(showsTraffic, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(showsBuildings, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(zoomLevel, CGFloat)
+
 RCT_EXPORT_VIEW_PROPERTY(maxZoomLevel, CGFloat)
+
 RCT_EXPORT_VIEW_PROPERTY(minZoomLevel, CGFloat)
+
 RCT_EXPORT_VIEW_PROPERTY(zoomEnabled, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(scrollEnabled, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(rotateEnabled, BOOL)
+
 RCT_EXPORT_VIEW_PROPERTY(tiltEnabled, BOOL)
+
+RCT_EXPORT_VIEW_PROPERTY(mapType, MAMapType)
 
 @end
