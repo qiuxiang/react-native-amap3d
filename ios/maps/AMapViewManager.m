4b9ade690c8209654b1375e79342d56f1b6049e8
YBQ <yangbingqing051@pingan.com>
2017-11-22 17:07:54 +0800

    定位用户位置属性

--- ios/maps/AMapViewManager.m
+++ ios/maps/AMapViewManager.m
@@ -44,6 +44,7 @@ RCT_EXPORT_VIEW_PROPERTY(region, MACoordinateRegion)
 RCT_EXPORT_VIEW_PROPERTY(tilt, CGFloat)
 RCT_EXPORT_VIEW_PROPERTY(rotation, CGFloat)
 RCT_EXPORT_VIEW_PROPERTY(distanceFilter, CLLocationDistance)
+RCT_EXPORT_VIEW_PROPERTY(userTrackingMode, MAUserTrackingMode)
 
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onLongPress, RCTBubblingEventBlock)
