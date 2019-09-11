101bdc0663fd89219c695655746fb7f8fb44943c
Sunny Luo <sunnylqm@qq.com>
2019-09-11 21:35:03 +0800

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+// https://github.com/react-native-community/cli/blob/master/docs/configuration.md#libraries
+module.exports = {
+  dependency: {
+    platforms: {
+      ios: {},
+      android: {
+        sourceDir: "lib/android"
+      }
+    }
+  }
+};
