469920d7367be3592c18d424f08cde7b482feefd
黄波 <1104133609@qq.com>
2020-02-11 13:41:39 +0800

    andorid搜索修改

--- lib/android/src/main/java/cn/qiuxiang/react/amap3d/poi/AMapPOIKeywordsSearch.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/amap3d/poi/AMapPOIKeywordsSearch.kt
@@ -1,19 +1,12 @@
 package cn.qiuxiang.react.amap3d.poi
 
-import com.amap.api.services.core.PoiItem
-import com.amap.api.services.poisearch.PoiResult
 import com.amap.api.services.poisearch.PoiSearch
-import com.amap.api.services.poisearch.PoiSearch.OnPoiSearchListener
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.Arguments.createMap
+import com.facebook.react.bridge.*
 import com.facebook.react.bridge.Arguments.createArray
+import com.facebook.react.bridge.Arguments.createMap
 
 
-class AMapPOIKeywordsSearch internal constructor(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), OnPoiSearchListener {
+class AMapPOIKeywordsSearch internal constructor(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     private var poiSearch: PoiSearch? = null
 
     override fun getName(): String {
@@ -22,74 +15,66 @@ class AMapPOIKeywordsSearch internal constructor(private val reactContext: React
 
     @ReactMethod
     fun keywordsSearchWithOptions(options: ReadableMap, promise: Promise) {
-        var keywords = ""
-        if (options.hasKey("keywords")) {
-            keywords = options.getString("keywords")!!
-        } else {
-            promise.reject("", "keywords is empty")
-            return
-        }
-
-        var types = ""
-        if (options.hasKey("types")) {
-            types = options.getString("types")!!
-        }
-
-        var cityCode = ""
-        if (options.hasKey(("cityCode"))) {
-            cityCode = options.getString("cityCode")!!
-        }
-
-        var query = AMapPOIKeyWordsSearchReq(keywords, types, cityCode)
-
-        query.promise = promise
-
-        if (options.hasKey("pageSize")) {
-            query!!.pageSize = options.getInt("pageSize")
-        }
-
-        if (options.hasKey("pageNum")) {
-            query!!.pageNum = options.getInt("pageNum")
-        }
-
-        poiSearch = PoiSearch(this.reactContext, query)
-
-        poiSearch!!.setOnPoiSearchListener(this)
-
-        poiSearch!!.searchPOIAsyn()
-    }
-
-    override fun onPoiSearched(result: PoiResult, rCode: Int) {
-        if (1000 == rCode) {
-            val poiArray = createArray()
-            for (poi in result.pois) {
-                val poiMap = createMap()
-                poiMap.putString("uid", poi.poiId)
-                poiMap.putString("name", poi.title)
-                poiMap.putString("type", poi.typeDes)
-                poiMap.putString("typeCode", poi.typeCode)
-                poiMap.putDouble("latitude", poi.latLonPoint.latitude)
-                poiMap.putDouble("longitude", poi.latLonPoint.longitude)
-                poiMap.putString("address", poi.snippet)
-                poiMap.putString("tel", poi.tel)
-                poiMap.putInt("distance", poi.distance)
-                poiMap.putString("cityCode", poi.cityCode)
-                poiMap.putString("cityName", poi.cityName)
-                poiMap.putString("provinceCode", poi.provinceCode)
-                poiMap.putString("provinceName", poi.provinceName)
-                poiMap.putString("adCode", poi.adCode)
-                poiMap.putString("adName", poi.adName)
-                poiArray.pushMap(poiMap)
+        try {
+            var keywords = ""
+            if (options.hasKey("keywords")) {
+                keywords = options.getString("keywords")!!
+            } else {
+                promise.reject("", "keywords is empty")
+                return
             }
-            (result.query as AMapPOIKeyWordsSearchReq).promise!!.resolve(poiArray);
-        } else {
-            (result.query as AMapPOIKeyWordsSearchReq).promise!!.reject(rCode.toString(), "error");
+
+            var types = ""
+            if (options.hasKey("types")) {
+                types = options.getString("types")!!
+            }
+
+            var cityCode = ""
+            if (options.hasKey(("cityCode"))) {
+                cityCode = options.getString("cityCode")!!
+            }
+
+            var query = AMapPOIKeyWordsSearchReq(keywords, types, cityCode)
+
+
+            if (options.hasKey("pageSize")) {
+                query.pageSize = options.getInt("pageSize")
+            }
+
+            if (options.hasKey("pageNum")) {
+                query.pageNum = options.getInt("pageNum")
+            }
+
+            poiSearch = PoiSearch(this.reactContext, query)
+            val result = poiSearch!!.searchPOI()
+            if (result != null) {
+                val poiArray = createArray()
+                for (poi in result.pois) {
+                    val poiMap = createMap()
+                    poiMap.putString("uid", poi.poiId)
+                    poiMap.putString("name", poi.title)
+                    poiMap.putString("type", poi.typeDes)
+                    poiMap.putString("typeCode", poi.typeCode)
+                    poiMap.putDouble("latitude", poi.latLonPoint.latitude)
+                    poiMap.putDouble("longitude", poi.latLonPoint.longitude)
+                    poiMap.putString("address", poi.snippet)
+                    poiMap.putString("tel", poi.tel)
+                    poiMap.putInt("distance", poi.distance)
+                    poiMap.putString("cityCode", poi.cityCode)
+                    poiMap.putString("cityName", poi.cityName)
+                    poiMap.putString("provinceCode", poi.provinceCode)
+                    poiMap.putString("provinceName", poi.provinceName)
+                    poiMap.putString("adCode", poi.adCode)
+                    poiMap.putString("adName", poi.adName)
+                    poiArray.pushMap(poiMap)
+                }
+                promise.resolve(poiArray)
+            }
+        } catch (e: Exception) {
+            promise.reject("-1",e.message)
         }
 
     }
 
-    override fun onPoiItemSearched(item: PoiItem, rCode: Int) {
-        // TODO Auto-generated method stub
 
-    }
 }
