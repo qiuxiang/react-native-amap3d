641fdf7990c139bfaff45518caf49d6ecc8ba4ba
7c00 <i@7c00.cc>
2020-02-27 18:42:14 +0800

    Fix autolinking

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  project: { android: { sourceDir: "lib/android" } },
-  // dependencies: { "react-native-amap3d": { platforms: { android: { sourceDir: "../lib/android" } } } }
+  dependency: {
+    platforms: {
+      ios: { project: "lib/ios/react-native-amap3d.podspec" },
+      android: { sourceDir: "lib/android" }
+    }
+  },
+  dependencies: {
+    "react-native-amap3d": {
+      platforms: { ios: { podspecPath: __dirname + "/lib/ios/react-native-amap3d.podspec" } }
+    }
+  }
 };
