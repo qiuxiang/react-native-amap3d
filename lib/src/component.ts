6ee974786cc4487a2283c362ffecdb0e7d3ba0ac
7c00 <i@7c00.cc>
2021-10-26 18:30:32 +0800

    Update

--- lib/src/component.ts
+++ lib/src/component.ts
@@ -2,16 +2,13 @@
  * Base component, contains some utils
  */
 import { PureComponent } from "react";
-import { findNodeHandle, NativeSyntheticEvent, Platform, UIManager, View } from "react-native";
+import { findNodeHandle, UIManager } from "react-native";
 
-/**
- * @ignore
- */
 export default class Component<P> extends PureComponent<P> {
   /**
    * Must be defined in subclass if need to call native component method
    */
-  nativeComponent = "";
+  name = "";
 
   /**
    * Call native method
@@ -19,26 +16,8 @@ export default class Component<P> extends PureComponent<P> {
   call(name: string, params?: any[]) {
     const handle = findNodeHandle(this);
     if (handle) {
-      const command = UIManager.getViewManagerConfig(this.nativeComponent).Commands[name];
+      const command = UIManager.getViewManagerConfig(this.name).Commands[name];
       UIManager.dispatchViewManagerCommand(handle, command, params);
     }
   }
-
-  /**
-   * Generate event handlers
-   */
-  handlers = (events: string[]) =>
-    events.reduce((handlers, name) => {
-      View;
-      const handler = Reflect.get(this.props, name);
-      if (handler) {
-        if (Platform.OS === "android") {
-          name = name.replace(/^on/, "onAMap");
-        }
-        Reflect.set(handlers, name, (event: NativeSyntheticEvent<any>) =>
-          handler(event.nativeEvent)
-        );
-      }
-      return handlers;
-    }, {});
 }
