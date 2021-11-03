369caf1718cc62e8c2df239a93aec0be6556fee5
7c00 <i@7c00.cc>
2021-11-03 15:52:33 +0800

    优化地图类型切换示例

--- example/screens/map-types.tsx
+++ example/screens/map-types.tsx
@@ -1,28 +1,25 @@
 import { Picker } from "@react-native-picker/picker";
-import { useNavigation, useRoute } from "@react-navigation/native";
 import * as React from "react";
-import { useEffect } from "react";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { MapType, MapView } from "react-native-amap3d";
-import { NavigationProps } from "../types";
 
 export default () => {
-  const navigation = useNavigation<NavigationProps>();
-  const { params = { mapType: MapType.Standard } } = useRoute();
-  const mapType = Reflect.get(params, "mapType");
-  const headerRight = () => (
-    <Picker
-      mode="dropdown"
-      style={{ width: 120 }}
-      selectedValue={mapType}
-      onValueChange={(mapType) => navigation.setParams({ mapType })}
-    >
-      <Picker.Item label="标准" value={MapType.Standard} />
-      <Picker.Item label="卫星" value={MapType.Satellite} />
-      <Picker.Item label="导航" value={MapType.Navi} />
-      <Picker.Item label="夜间" value={MapType.Night} />
-      <Picker.Item label="公交" value={MapType.Bus} />
-    </Picker>
+  const [mapType, setMapType] = useState(MapType.Standard);
+  return (
+    <View style={StyleSheet.absoluteFill}>
+      <MapView style={{ flex: 1 }} mapType={mapType} />
+      <Picker
+        style={{ backgroundColor: "#fff" }}
+        selectedValue={mapType}
+        onValueChange={setMapType}
+      >
+        <Picker.Item label="标准" value={MapType.Standard} />
+        <Picker.Item label="卫星" value={MapType.Satellite} />
+        <Picker.Item label="导航" value={MapType.Navi} />
+        <Picker.Item label="夜间" value={MapType.Night} />
+        <Picker.Item label="公交" value={MapType.Bus} />
+      </Picker>
+    </View>
   );
-  useEffect(() => navigation.setOptions({ headerRight }));
-  return <MapView mapType={mapType} />;
 };
