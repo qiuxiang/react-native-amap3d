0b9f91ad026c1bff639e93175b6e8d455642cbb8
7c00 <i@7c00.cc>
2022-05-17 23:36:49 +0800

    Fix ios privacy

--- lib/ios/Modules/SdkModule.swift
+++ lib/ios/Modules/SdkModule.swift
@@ -6,6 +6,8 @@ class AMapSdk: NSObject {
 
   @objc func initSDK(_ apiKey: String) {
     AMapServices.shared().apiKey = apiKey
+    MAMapView.updatePrivacyAgree(AMapPrivacyAgreeStatus.didAgree)
+    MAMapView.updatePrivacyShow(AMapPrivacyShowStatus.didShow, privacyInfo: AMapPrivacyInfoStatus.didContain)
   }
 
   @objc func getVersion(_ resolve: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
