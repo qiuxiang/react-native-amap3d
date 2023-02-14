2b73cea81bd06e9957e9e0af7b236acaf207bf6b
7c00 <i@7c00.cc>
2023-02-14 09:57:42 +0800

    Fix ios project config

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   dependency: {
     platforms: {
-      ios: { project: "lib/ios/react-native-amap3d.podspec" },
       android: { sourceDir: "lib/android" },
     },
   },
@@ -9,7 +8,6 @@ module.exports = {
     "react-native-amap3d": {
       root: __dirname,
       platforms: {
-        ios: { podspecPath: __dirname + "/lib/ios/react-native-amap3d.podspec" },
         android: {
           sourceDir: __dirname + "/lib/android",
           packageImportPath: "import qiuxiang.amap3d.AMap3DPackage;",
